8c8bdd8beac0e075043eab8085f18a96f299358c
zhouxin <635029155@qq.com>
2019-12-28 17:10:53 +0800

    Ex3.2 exceed.cpp

---
+++ Ex3.2/Ex3.2.cpp
@@ -0,0 +1,46 @@
+﻿// Ex3.2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#define ZERO 0
+#include<climits>
+using namespace std;
+
+int main()
+{
+	short sam = SHRT_MAX;	//initialize a variable to max value
+	unsigned short sue = sam;	// okay if variable sam already define
+
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl << "Add $1 to each account." << endl << "Now ";
+	sam += 1;
+	sue += 1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposite.\nPoor Sam!" << endl;
+
+	sam = ZERO;
+	sue = ZERO;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue << endl;
+	cout << " dollars deposited." << endl;
+	cout << "Take $1 from each account." << endl << "Now ";
+	sam = sam - 1;
+	sue = sue - 1;
+
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl << "Lucky Sue!" << endl;
+
+	system("pause");
+	return 0;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
