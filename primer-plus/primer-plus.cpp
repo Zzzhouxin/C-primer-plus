0b265b5b6323e226a4a7ff13fe4bbcacb04a83cd
zhouxin <635029155@qq.com>
2019-12-28 16:33:27 +0800

    Ex3.1 limits.cpp

--- primer-plus/primer-plus.cpp
+++ primer-plus/primer-plus.cpp
@@ -2,12 +2,35 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;	//initialize n_init to max int value
+	long n_long = LONG_MAX;		//symbols defined in climits file
+	long n_llong = LLONG_MAX;
 
+	//sizeof operator yields size of type of variable
+	cout << "int is " << sizeof(int) << " bytes." << endl;
+	cout << "short is " << sizeof(n_short) << " bytes." << endl;
+	cout << "long is " << sizeof(n_long) << " bytes." << endl;
+	cout << "llong is " << sizeof(n_llong) << " bytes." << endl;
+
+	cout << endl;
+
+	cout << "Maximum values:" << endl;
+	cout << "int: " << n_int << endl;
+	cout << "short: " << n_short << endl;
+	cout << "long: " << n_long << endl;
+	cout << "long long: " << n_llong << endl << endl;
+
+	cout << "Minimum int value = " << INT_MIN << endl;
+	cout << "Bits per byte = " << CHAR_BIT << endl;
+
+	return 0;
+
+}
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
